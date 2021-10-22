a0ca867018a54ed3a489e944a23ac2689aebfe79
ncclementi <natyclementi@gmail.com>
2021-10-22 16:00:54 -0400

    add google-auth to req and add test req

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
     install_requires=open("requirements.txt").read().strip().split("\n"),
-    extras_require={"test": ["pytest"]},
+    extras_require={"test": ["pytest", "pandas-gbq", "distributed"]},
     include_package_data=True,
     zip_safe=False,
 )
