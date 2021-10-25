99596687071d044e618f5895d72dd85b5772f94c
ncclementi <natyclementi@gmail.com>
2021-10-25 15:08:43 -0400

    fix deadline exceeded exception

--- dask_bigquery/tests/test_core.py
+++ dask_bigquery/tests/test_core.py
@@ -83,7 +83,7 @@ def test_read_kwargs(dataset, client):
         read_kwargs={"timeout": 1e-12},
     )
 
-    with pytest.raises(Exception, match="504 Deadline Exceeded"):
+    with pytest.raises(Exception, match="Deadline Exceeded"):
         ddf.compute()
 
 
