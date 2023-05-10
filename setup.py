6b4a07eae3d3dcfbf355747d42975abbe91b58a9
Naty Clementi <natyclementi@gmail.com>
2023-05-10 13:35:40 -0400

    Remove pandas-gbq from extras and fix typo (#55)

--- setup.py
+++ setup.py
@@ -8,16 +8,14 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="dask-bigquery",
     version="2022.05.0",
-    description="Dask + BigQuery intergration",
+    description="Dask + BigQuery integration",
     license="BSD",
     packages=["dask_bigquery"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.8",
     install_requires=open("requirements.txt").read().strip().split("\n"),
-    extras_require={
-        "test": ["pytest", "pandas-gbq<=0.15", "distributed", "google-auth>=1.30.0"]
-    },
+    extras_require={"test": ["pytest", "distributed", "google-auth>=1.30.0"]},
     include_package_data=True,
     zip_safe=False,
 )
